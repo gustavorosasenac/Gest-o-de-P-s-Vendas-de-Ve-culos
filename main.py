7adaa658712f7ea02d6a9fb83fa70ef59b37648d
gustavorosasenac <gustavo.rosa11@alunos.sc.senac.br>
2025-06-12 15:24:07 -0300

    inicio do menu

--- main.py
+++ main.py
@@ -1,62 +1,84 @@
 import flet as ft
 
-def menu(page: ft.Page):
-    page.title = "Menu Principal"
+def menu_principal(page: ft.Page):
+    page.tittle = "Menu Principal"
     page.theme_mode = ft.ThemeMode.DARK
 
-    def clique(e):
-        page.dialog = ft.AlertDialog(
-            title=ft.Text(f"Você clicou em: {e.control.text}"),
-            open=True
-        )
-        page.update()
 
-    titulo = ft.Text("🚘 Sistema de Veículos", size=40, weight=ft.FontWeight.BOLD, color="white")
+    def mostrar_cadastros(e):
+        page.clean()
+        cadastros(page)
 
-    botao_cadastrar = ft.ElevatedButton(
-        text="Cadastrar Veículo",
-        icon=ft.Icon(name="add_circle"),
+    def mostrar_consultas(e):
+        page.clean()
+        consultas(page)
+
+
+    titulo = ft.Text("🚘 Sistema de Veículos", size = 40, weight=ft.FontWeight.BOLD, color = "black")
+
+    botao_cadastros = ft.ElevatedButton(
+        text="Cadastros",
+        icon=ft.Icon(name = "assigment"),
         width=400,
-        on_click=clique
-    )
-
-    botao_consultar = ft.ElevatedButton(
-        text="Consultar Veículos",
-        icon=ft.Icon(name="search"),
+        on_click = mostrar_cadastros)
+    
+    botao_consultas = ft.ElevatedButton(
+        text="Consultas",
+        icon=ft.Icon(name = "search"),
         width=400,
-        on_click=clique
-    )
-
+        on_click = mostrar_consultas)
+        
     conteudo = ft.Column(
-        [
-            titulo,
-            ft.Divider(),
-            botao_cadastrar,
-            botao_consultar
-        ],
+        [titulo, ft.Divider(), botao_cadastros, botao_consultas],
         alignment=ft.MainAxisAlignment.CENTER,
-        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+    
+    page.add(
+        ft.Stack(
+            controls=[
+                ft.Image(
+                    src="https://img.odcdn.com.br/wp-content/uploads/2024/03/shutterstock_1082263868-1.jpg",
+                    fit=ft.ImageFit.COVER,
+                    width=page.width,
+                    height=page.height
+                ),
+                ft.Container(
+                    content=conteudo,
+                    alignment=ft.alignment.center,
+                    padding=40,
+                    bgcolor="#00000088",
+                    border_radius=10
+                )
+            ],
+            expand=True
+        )
     )
 
-    layout = ft.Stack(
-        controls=[
-            ft.Image(
-                src="https://previews.123rf.com/images/misign/misign2204/misign220400001/184469781-a-old-yellow-tricycle-car-is-a-three-wheeled-light-commercial-vehicle-produced-since-1948-in-italy.jpg",
-                fit=ft.ImageFit.COVER,
-                width=page.width,
-                height=page.height
-            ),
-            ft.Container(
-                content=conteudo,
-                alignment=ft.alignment.center,
-                padding=40,
-                bgcolor="#00000088",  # Preto com 53% de opacidade
-                border_radius=10
-            )
-        ],
-        expand=True
-    )
+def cadastros(page: ft.Page):
+    def voltar_menu(e):
+        page.clean()
+        menu_principal(page)
+    page.tittle = "Tela de Cadastros"
+    page.add(
+        ft.Column([
+            ft.Text("Cadastros", size = 50, weight=ft.FontWeight.BOLD, color = "white"),
+            ft.ElevatedButton(text = "Voltarao Menu", icon=ft.Icons.ARROW_BACK, on_click=voltar_menu),
+            ft.ElevatedButton(text="Cadastros", icon=ft.Icon(name = "assigment"), width=400)],
 
-    page.add(layout)
+            alignment = ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,))
+    
+def consultas(page: ft.Page):
+    def voltar_menu(e):
+        page.clean()
+        menu_principal(page)
+    page.tittle = "Tela de Consultas"
+    page.add(
+        ft.Column([
+            ft.Text("Tela de Consultas", size=30, color = "white"),
+            ft.ElevatedButton(text = "Voltarao Menu", icon=ft.Icons.ARROW_BACK, on_click=voltar_menu)],
 
-ft.app(target=menu)
+            alignment = ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,))
+    
+ft.app(target = menu_principal)
