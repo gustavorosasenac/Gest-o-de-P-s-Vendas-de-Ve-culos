d25f850e1aed7d28b5b1b32ec8c1ef3b80d18eeb
gustavorosasenac <gustavo.rosa11@alunos.sc.senac.br>
2025-06-13 15:07:42 -0300

    menu consultas

--- main.py
+++ main.py
@@ -139,9 +139,55 @@ def cadastros(page: ft.Page):
             expand=True
         )
     )
+  
+def consultas(page: ft.Page):
+    page.tittle = "Menu de Consultas"
+    page.theme_mode = ft.ThemeMode.DARK
+    def voltar_menu(e):
+        page.clean()
+        menu_principal(page)
+    def consultar_veiculos(e):
+        pass
 
+    titulo = ft.Text("Consultas", size = 40, weight=ft.FontWeight.BOLD, color = "black")
 
+    botao_consultar_veiculos = ft.ElevatedButton(
+        text="Consultar Veículos",
+        icon=ft.Icon(name = "search"),
+        width=400,
+        on_click = consultar_veiculos)
 
+    botao_voltar_menu = ft.ElevatedButton(
+        text="Voltar ao Menu Principal",
+        icon=ft.Icon(name = "arrow_back"),
+        width=400,
+        on_click = voltar_menu)
+        
+    conteudo = ft.Column(
+        [titulo, botao_consultar_veiculos, botao_voltar_menu],
+        alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+    
+    page.add(
+        ft.Stack(
+            controls=[
+                ft.Image(
+                    src="https://img.odcdn.com.br/wp-content/uploads/2024/03/shutterstock_1082263868-1.jpg",
+                    fit=ft.ImageFit.COVER,
+                    width=page.width,
+                    height=page.height
+                ),
+                ft.Container(
+                    content=conteudo,
+                    alignment=ft.alignment.center,
+                    padding=40,
+                    bgcolor="#00000088",
+                    border_radius=10
+                )
+            ],
+            expand=True
+        )
+    )
 
 
 
@@ -243,18 +289,6 @@ def cadastros_de_veiculo(page: ft.Page):
         alignment = ft.MainAxisAlignment.CENTER,
         horizontal_alignment= ft.CrossAxisAlignment.CENTER,))
 
-   
-def consultas(page: ft.Page):
-    def voltar_menu(e):
-        page.clean()
-        menu_principal(page)
-    page.title = "Tela de Consultas"
-    page.add(
-        ft.Column([
-            ft.Text("Tela de Consultas", size=30, color = "white"),
-            ft.ElevatedButton(text = "Voltar ao Menu", icon=ft.Icons.ARROW_BACK, on_click=voltar_menu)],
 
-            alignment = ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,))
     
 ft.app(target=menu_principal, view=ft.AppView.FLET_APP)
