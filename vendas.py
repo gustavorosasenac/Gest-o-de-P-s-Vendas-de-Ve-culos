618d1617f8d34bcc28d9f117984f477a2feccefe
gustavorosasenac <gustavo.rosa11@alunos.sc.senac.br>
2025-06-17 21:44:44 -0300

    Menu de vendas adicionado

--- vendas.py
+++ vendas.py
@@ -1,9 +1,9 @@
 from DB import Base
-from sqlalchemy import Column, Integer, String, Date, Float
+from sqlalchemy import Column, Integer, String, Date
 
 class Vendas(Base):
     __tablename__ = "vendas"
     id = Column(Integer, primary_key=True, autoincrement=True)
     data_venda = Column(Date, nullable=False)
     comprador = Column(String(100), nullable=False)
-    valor = Column(Float, nullable=False)
+    valor = Column(String(100), nullable=False)
