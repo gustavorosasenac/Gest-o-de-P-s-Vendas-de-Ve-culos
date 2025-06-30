51dfed00b2e7cc27220a8d4c19c478d8380da021
gustavorosasenac <gustavo.rosa11@alunos.sc.senac.br>
2025-06-30 19:30:18 -0300

    Austes feitos

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import flet as ft
 from DB.Database import Base, engine
 from Models.visual import Menu_principal
+
 #cria o BD
 Base.metadata.create_all(engine)
+
 #Chama o menu principal
 def main(page: ft.Page):
     Menu_principal(page)
+    
 #Inicia a aplicação chamando o main
 if __name__ == "__main__":
     ft.app(target=main)
