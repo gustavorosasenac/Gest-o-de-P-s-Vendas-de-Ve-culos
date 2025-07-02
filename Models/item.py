c7192a81a6e2cbd945b073ad63ce32f5b0179fb9
gabrielkreich <gabriel.kreich@alunos.sc.senac.br>
2025-07-01 22:00:14 -0300

    Initens

--- Models/item.py
+++ Models/item.py
@@ -1,5 +1,6 @@
 import flet as ft
 from DB.Database import session, Base, engine
+from DB.Tables.table_item import Itens
 
 Base.metadata.create_all(engine)
 
@@ -41,4 +42,120 @@ def criar_botao(texto, icone, funcao, cor=ft.Colors.BLUE_700):
                 animate=ft.Animation(300, "easeInOut")
             )
 
-def cadastrar_item(page: ft.Page):
+#Inicia o cadastro de item
+def cadastro_de_itens(page: ft.Page):
+    # Campos do formulário
+    nome = ft.TextField(label="Nome", width=400)
+    preço  = ft.TextField(label="Preço", width=400)
+    quantidade = ft.TextField(label="Quantidade", width=400)
+
+    # Função para cadastrar ‘item’
+    def cadastro_de_itens(e):
+        if not all([nome.value, preço.value, quantidade.value]):
+            dlg_erros.content = ft.Text("Preencha todos os campos!", color="red", size=20)
+            page.open(dlg_erros)
+            dlg_erros.open = True
+            return
+        if not quantidade.value.isdigit():
+            dlg_erros.content = ft.Text("Quantidade deve ser um número inteiro", color="red", size=20)
+            page.open(dlg_erros)
+            dlg_erros.open = True
+            return
+
+        verificar_cadastro = session.query(Itens).filter(
+            Itens.nome == nome.value,
+            Itens.preço == preço.value,
+            Itens.quantidade == int(quantidade.value),
+        ).first()
+
+        if verificar_cadastro:
+            dlg_ja_cadastrado.content = ft.Text("Item já cadastrado", color="red", size=20)
+            page.open(dlg_ja_cadastrado)
+            dlg_ja_cadastrado.open = True
+            return
+        else:
+            novo_item = Itens(
+                nome = nome.value,
+                preço = preço.value,
+                quantidade = int(quantidade.value),
+                )
+
+            session.add(novo_item)
+            session.commit()
+
+            dlg_sucesso.content = ft.Text("Item cadastrado com sucesso!", color="green", size=20)
+            page.open(dlg_sucesso)
+            dlg_sucesso.open = True
+
+    botao_cadastrar = criar_botao("Cadastrar", ft.Icons.ADD, cadastro_de_itens, ft.Colors.TEAL_700)
+
+    # Retorna os controles do formulário
+    return ft.Container(
+        content=ft.Column(
+            [
+                ft.Text("Novo item", size=50, weight=ft.FontWeight.BOLD, color="white"),
+                nome,
+                preço,
+                quantidade,
+                ft.Divider(height=40, color=ft.Colors.TRANSPARENT),
+                botao_cadastrar,
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        ),
+        bgcolor=ft.Colors.with_opacity(0.90, ft.Colors.BLACK),
+        border_radius=20,
+        width=500,
+        alignment=ft.alignment.center,
+        margin=ft.margin.symmetric(vertical=100),
+        padding=20
+    )
+
+
+def listar_itens(page: ft.Page):
+    itens = session.query(Itens).all()
+
+    if not Itens:
+        dlg_erros.content = ft.Text("Nenhum item cadastrado", color="red", size=20)
+        page.open(dlg_erros)
+        dlg_erros.open = True
+        page.update()
+        return
+    # ft.ListView vai exibir em formato de lista
+
+    lista_item = ft.ListView(
+        controls=[
+            ft.Container(
+                content=ft.Text(f"ID: {v.id} | Nome: {v.nome} | Preço: {v.preço} | ",
+                                size=16,
+                                color=ft.Colors.WHITE),
+                padding=10,
+                border=ft.border.all(1, ft.Colors.GREY_800),
+                margin=ft.margin.only(bottom=5))
+            for v in item
+        ],
+        width=900,
+        spacing=5,
+        padding=10
+    )
+
+    return ft.Container(
+        content=ft.Column([
+            ft.Text("  Itens Cadastrados",
+                    size=40,
+                    weight=ft.FontWeight.BOLD,
+                    color="white",
+                    text_align=ft.TextAlign.LEFT),  # Alinhamento do título à esquerda
+            ft.Divider(height=20),
+            lista_item
+        ],
+            alignment=ft.MainAxisAlignment.START,  # Alinha no topo
+            horizontal_alignment=ft.CrossAxisAlignment.START  # Alinha tudo à esquerda
+        ),
+        bgcolor=ft.Colors.with_opacity(0.90, ft.Colors.BLACK),
+        border_radius=20,
+        width=1000,
+        padding=ft.padding.only(left=20, top=20, right=20, bottom=20),  # Padding igual em todos os lados
+        margin=ft.margin.only(left=20),  # Margem reduzida à esquerda
+        alignment=ft.alignment.top_left  # Alinha o container no topo esquerdo
+    )
