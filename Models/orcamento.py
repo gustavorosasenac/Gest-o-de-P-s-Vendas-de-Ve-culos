04bf9ad707bda2e48bf125de18a3fea3df4360b0
gustavorosasenac <gustavo.rosa11@alunos.sc.senac.br>
2025-07-02 14:52:54 -0300

    menu diagnostico e orcamento feito

---
+++ Models/orcamento.py
@@ -0,0 +1,16 @@
+import flet as ft
+from DB.Database import session
+
+
+
+def cadastro_de_orcamento(page: ft.Page):
+    pass
+
+def listar_orcamento(page: ft.Page):
+    pass
+
+def alterar_orcamento(page: ft.Page):
+    pass
+
+def excluir_orcamento(page: ft.Page):
+    pass
