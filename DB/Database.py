c61052969f1bc2e9cf3d05a8eeca2b96f8906d5a
gustavorosasenac <gustavo.rosa11@alunos.sc.senac.br>
2025-07-09 15:22:38 -0300

    add modificador e compilador

--- DB/Database.py
+++ DB/Database.py
@@ -2,13 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
 load_dotenv()
 
 usuario = os.getenv("USUARIO")
 senha = os.getenv("SENHA")
 
-engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@localhost:3306/veiculos")
+#engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@localhost:3306/veiculos")
+
+
+BASE_DIR = Path(__file__).parent
+DATABASE_URL = f"sqlite:///{BASE_DIR}/veiculos.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
--- README.md
+++ README.md
@@ -7,5 +7,8 @@ Interface grafico sera usada "flet"
 Bibliotecas necessarias: pip install flet, mysql, pymysql, sqlachemy, cryptography, werkzeug
 
 
+comando para compilar
 
+pyinstaller --onefile --windowed --name "AppVeiculos" --add-data "DB;DB" --add-data 
+"assets;assets"  --hidden-import "DB.Database" --hidden-import "sqlalchemy" --hidden-import "sqlite3" main.py
 
