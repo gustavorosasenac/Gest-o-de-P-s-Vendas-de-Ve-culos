341093fdd461481e3f01fb212a1a3bd88c0a9fa7
gustavorosasenac <gustavo.rosa11@alunos.sc.senac.br>
2025-07-15 17:33:44 -0300

    add informações do banco

--- DB/Database.py
+++ DB/Database.py
@@ -9,16 +9,16 @@ load_dotenv()
 usuario = os.getenv("USUARIO")
 senha = os.getenv("SENHA")
 
-engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@localhost:3306/veiculos")
+#engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@localhost:3306/veiculos")
 
 
-'''BASE_DIR = Path(__file__).parent
+BASE_DIR = Path(__file__).parent
 DATABASE_URL = f"sqlite:///{BASE_DIR}/veiculos.db"
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
-)'''
+)
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
